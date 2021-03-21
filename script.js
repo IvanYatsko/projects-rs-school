d9651019210c48cac3cdf01d121967f505a6bd59
Yatsko Ivan <ivanyatsko@mail.ru>
2021-03-22 02:41:31 +0300

    feat: ready progect

--- script.js
+++ script.js
@@ -1,9 +1,54 @@
 window.onload = function() {
     const PIANO = document.querySelector('.piano');
     const PIANOKEY = document.querySelectorAll('.piano-key');
+    const CHANGE = document.querySelectorAll('.btn-container .btn');
+    const BUTTONTOGGLE = document.querySelector('.fullscreen');
+
+    const toggleFullScreen = () => {
+        if (!document.fullscreenElement) {
+            document.documentElement.requestFullscreen();
+        } else {
+          if (document.exitFullscreen) {
+            document.exitFullscreen();
+          }
+        }
+      }
+
+    const changeButton = () => {
+        CHANGE.forEach((item) => {
+            if (item.classList.contains('btn-active') && item.getAttribute('data-button') == 'notes') {
+                PIANOKEY.forEach((item) => {
+                    item.classList.remove('piano-key-letter');
+                    item.classList.add('piano-key-note');
+                })
+            } else if (item.classList.contains('btn-active') && item.getAttribute('data-button') == 'letters') {
+                PIANOKEY.forEach((item) => {
+                    item.classList.remove('piano-key-note');
+                    item.classList.add('piano-key-letter');
+                });
+            }
+        });
+    }
+
+    const clickButton = (event) => {
+        CHANGE.forEach((elem) => {
+            elem.classList.remove('btn-active');
+        });
+        event.target.classList.add('btn-active');
+        changeButton();
+    }
+
+    const playSound = (note) => {
+        let src = `./assets/audio/${note}.mp3`;
+        const audio = new Audio();
+        audio.src = src;
+        audio.currentTime = 0;
+        audio.play();
+    }
 
     const startPlay = (event) => {
         event.target.classList.add('piano-key-active');
+        playSound(event.target.getAttribute('data-note'));
     }
 
     const stopPlay = (event) => {
@@ -13,10 +58,11 @@ window.onload = function() {
     const clickPiano = (event) => {
         if (event.target.classList.contains('piano-key')) {
             event.target.classList.add('piano-key-active');
+            playSound(event.target.getAttribute('data-note'));
             PIANOKEY.forEach((item) => {
                 item.addEventListener('mouseover', startPlay);
                 item.addEventListener('mouseout', stopPlay);
-            })
+            });
         }
     }
 
@@ -31,7 +77,8 @@ window.onload = function() {
     const clickKeyboard = (event) => {
         PIANOKEY.forEach((item) => {
             if (item.getAttribute('data-letter') == event.code.slice(3)) {
-                item.classList.add('piano-key-active')
+                item.classList.add('piano-key-active');
+                playSound(item.getAttribute('data-note'));
             }
         })
     }
@@ -41,6 +88,11 @@ window.onload = function() {
         })
     }
 
+    changeButton();
+    CHANGE.forEach((item) => {
+        item.addEventListener('click', clickButton);
+    })
+    BUTTONTOGGLE.addEventListener('click', toggleFullScreen);
     PIANO.addEventListener('mousedown', clickPiano, false);
     window.addEventListener('mouseup', unclickPiano);
 
