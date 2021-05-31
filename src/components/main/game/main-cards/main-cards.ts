39ed69b27244e0b4dedf09a638be6feb26dbec57
Yatsko Ivan <ivanyatsko@mail.ru>
2021-06-01 00:54:18 +0300

    feat: add timer

--- src/components/main/game/main-cards/main-cards.ts
+++ src/components/main/game/main-cards/main-cards.ts
@@ -4,6 +4,24 @@ import { Card } from './card/card';
 
 const SHOW_TIME = 5;
 
+function startTimer() {
+  const timer = document.querySelector('.main-timer');
+
+  let timeValue = 0;
+  function drawTimer() {
+    if (timer == null) throw Error('Element not found');
+    timeValue += 1000;
+    const d = new Date(timeValue);
+    const strTimer = `0${d.getMinutes()}:${d.getSeconds() < 10 ? `0${d.getSeconds()}` : d.getSeconds()}`;
+    timer.innerHTML = strTimer;
+    timer?.classList.add('text');
+    timer?.classList.add('main-timer__text');
+  }
+  setInterval(() => {
+    drawTimer();
+  }, 1000);
+}
+
 export class MainCards extends BaseComponent {
   private cards: Card[] = [];
 
@@ -21,6 +39,7 @@ export class MainCards extends BaseComponent {
     this.cards.forEach((card) => this.element.appendChild(card.element));
     setTimeout(() => {
       this.cards.forEach((card) => card.flipToBack());
+      startTimer();
     }, SHOW_TIME * 1000);
   }
 }
